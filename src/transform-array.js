c25ca13ab6bad665d473f7bc106dbf35c4e15559
Answerzu <bbeijing92@gmail.com>
2020-10-28 18:04:40 +0300

    feat: solve hanoi and what season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i - 1] == '--discard-next') {
+            newArr.pop();
+            newArr[i] = "";
+        } else if (arr[i] == '--discard-prev') {
+            newArr.pop();
+        } else if (arr[i] == '--double-next') {
+            newArr.push(arr[i + 1]);
+        } else if (arr[i] == '--double-prev') {
+            newArr.push(arr[i - 1]);
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    newArr = newArr.filter(function(el) {
+        return el != null && el != "";
+    });
+
+    return newArr;
+
 };
