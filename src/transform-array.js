94a914f8a80ae7c65be9ab0ecc1deb2c991876ef
Answerzu <bbeijing92@gmail.com>
2020-10-28 21:07:57 +0300

    feat:add js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-    let newArr = [];
+    if (Object.prototype.toString.call(arr) !== '[object Array]') {
+        throw new Error();
+    } else if (arr == []) {
+        let myArr;
+        return myArr = [];
+    } else {
+        let newArr = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i - 1] == '--discard-next') {
-            newArr.pop();
-            newArr[i] = "";
-        } else if (arr[i] == '--discard-prev') {
-            newArr.pop();
-        } else if (arr[i] == '--double-next') {
-            newArr.push(arr[i + 1]);
-        } else if (arr[i] == '--double-prev') {
-            newArr.push(arr[i - 1]);
-        } else {
-            newArr.push(arr[i]);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i - 1] == '--discard-next') {
+                newArr.pop();
+                newArr[i] = "";
+            } else if (arr[i] == '--discard-prev') {
+                newArr.pop();
+            } else if (arr[i] == '--double-next') {
+                newArr.push(arr[i + 1]);
+            } else if (arr[i] == '--double-prev') {
+                newArr.push(arr[i - 1]);
+            } else {
+                newArr.push(arr[i]);
+            }
         }
-    }
-    newArr = newArr.filter(function(el) {
-        return el != null && el != "";
-    });
+        newArr = newArr.filter(function(el) {
+            return el != null && el != "" && typeof el === "number";
+        });
 
-    return newArr;
+        return newArr;
+    }
 
 };
